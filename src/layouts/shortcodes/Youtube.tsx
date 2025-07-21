10d05491630deaa07aa45acfbfe81f56a0b05561
Enmanoell Mosley <enmamosley@gmail.com>
2025-07-21 14:56:10 -0600

    fix Youtube

--- src/layouts/shortcodes/Youtube.tsx
+++ src/layouts/shortcodes/Youtube.tsx
@@ -1,20 +1,33 @@
-import React, { useEffect } from "react";
+// Dynamic import que nunca rompe en build
+import React from "react";
+import { useEffect, useState } from "react";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
-const Youtube = ({
-  id,
-  title,
-  ...rest
-}: {
+interface YoutubeProps {
   id: string;
   title: string;
   [key: string]: any;
-}) => {
+}
+
+const Youtube: React.FC<YoutubeProps> = ({ id, title, ...rest }) => {
+  const [LiteYouTubeEmbed, setLiteYouTubeEmbed] = useState<any>(null);
+
   useEffect(() => {
-    import("@justinribeiro/lite-youtube");
+    import("react-lite-youtube-embed").then((mod) =>
+      setLiteYouTubeEmbed(() => mod.default || mod)
+    );
   }, []);
 
-  // @ts-ignore
-  return <lite-youtube videoid={id} videotitle={title} {...rest} />;
+  if (!LiteYouTubeEmbed) return null;
+
+  return (
+    <LiteYouTubeEmbed
+      wrapperClass="yt-lite rounded-lg"
+      id={id}
+      title={title}
+      {...rest}
+    />
+  );
 };
 
 export default Youtube;
